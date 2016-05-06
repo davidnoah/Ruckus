d4dc613fe9c7a43c617b5ed4f871113178b582b4
davidnoah <david.t.noah@gmail.com>
2016-05-06 15:25:54 -0700

    update version

---
+++ webpack.config.prod.js
@@ -0,0 +1,39 @@
+var path = require("path");
+var webpack require("webpack");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/ruckus.jsx",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
+  devtool: "source-map",
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      },
+    ]
+  }
+};
