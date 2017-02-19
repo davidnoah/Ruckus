49672cac815547c844308f6c2305ce9d025f416b
davidnoah <david.t.noah@gmail.com>
2017-02-19 12:47:20 -0800

    browserify

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,8 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+  config.browserify_rails.commandline_options = "-t [ babelify --presets [ es2015 ] ]"
+
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
